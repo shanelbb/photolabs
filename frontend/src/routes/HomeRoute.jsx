2f57024826294f477c6ecd4d2c24d1b148c7528a
Shanel Beebe <shanel.beebe@publicisna.com>
2024-06-08 21:53:39 -0600

    Refactor app to use mock data for photos and topics.

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,11 +3,12 @@ import PhotoList from "components/PhotoList";
 import TopNavigation from "components/TopNavigationBar";
 import "../styles/HomeRoute.scss";
 
-const HomeRoute = () => {
+const HomeRoute = props => {
+  const {photos, topics} = props;
   return (
     <div className='home-route'>
-      <TopNavigation />
-      <PhotoList />
+      <TopNavigation topics={topics} />
+      <PhotoList photos={photos} />
     </div>
   );
 };
